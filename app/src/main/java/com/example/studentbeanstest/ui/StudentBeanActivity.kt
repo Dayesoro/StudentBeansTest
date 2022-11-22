bcac760924ef4d859131492164636920f0bd4443
Daniel Ayesoro <dayesoro@gmail.com>
2022-11-22 17:49:11 +0000

    created views

---
+++ app/src/main/java/com/example/studentbeanstest/ui/StudentBeanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.studentbeanstest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentBeanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_bean)
+    }
+}
