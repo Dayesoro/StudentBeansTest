8fd23716ad13af9f161a80c67e227faa09d2665e
Daniel Ayesoro <dayesoro@gmail.com>
2022-11-23 20:17:15 +0000

    implemented login validation

--- app/src/main/java/com/example/studentbeanstest/ui/LoginFragment.kt
+++ app/src/main/java/com/example/studentbeanstest/ui/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.studentbeanstest.ui
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,20 +13,32 @@ import com.example.studentbeanstest.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-lateinit var binding:FragmentLoginBinding
+    lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         setupValidation()
         return binding.root
     }
 
     private fun setupValidation() {
         binding.logInButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_listFragment)
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+
+            if (email.isEmpty()) {
+                binding.emailEditText.error = "Email cannot be empty"
+            } else if (password.isEmpty()) {
+                binding.passwordEditText.error = "Password cannot be empty"
+            } else {
+                findNavController().navigate(R.id.action_loginFragment_to_listFragment)
+            }
+
+
         }
     }
 
